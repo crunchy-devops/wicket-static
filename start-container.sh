50502f87eeff30c1231281ee95e082e5a2d4aa10
Herve <dockerlite@gmail.com>
2022-05-16 17:01:42 +0200

    Added start-container.sh

---
+++ start-container.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+CONTAINER_NAME="wicket"
+OLD="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME")"
+if [ -n "$OLD" ]; then
+  docker rm -f $OLD
+fi
+docker run -d --name wicket -p 8090:8080 wicket-app
